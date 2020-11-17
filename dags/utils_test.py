51413f51a1227972e7a5a075146fedbd23f75327
codigube <codigube@gmail.com>
2020-11-17 14:54:55 +0100

    Failed the python unit test

--- dags/utils_test.py
+++ dags/utils_test.py
@@ -5,7 +5,7 @@ from utils import *
 class TestUtils(unittest.TestCase):
 
     def test_say_hello(self):
-        self.assertEqual(say_hello('foo'), 'Hello foo!')
+        self.assertEqual(say_hello('foo'), 'Hello foo!!')
 
 if __name__ == '__main__':
     unittest.main()
